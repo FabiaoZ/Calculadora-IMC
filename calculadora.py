ef1c175e101b1b9eef1dba14362457b6827910b0
FabiaoZ <fabiovga9596@gmail.com>
2022-10-24 17:20:10 -0300

    Calculadora IMC
    hoje programamos nome, idade, altura e outros dados.

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite sua peso: "))
+
+print("-" * 30)
+print("Os dados coletados foram: ")
+print("Seu nome", nome)
+print("Seu idade", idade, "anos")
+print("Sua altura", altura, "m")
+print("Seu peso", peso, "Kg")
+print("-" * 30)
+
+
